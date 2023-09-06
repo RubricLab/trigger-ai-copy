4e3a6aafc7295791e2fbd99a9079da1715759c39
tedspare <ted.spare@gmail.com>
2023-09-06 16:45:22 -0400

    Remove headless browser dependencies for now

--- next.config.js
+++ next.config.js
@@ -3,13 +3,6 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.externals.push("chrome-aws-lambda", "puppeteer-core");
-    }
-
-    return config;
-  },
 };
 
 module.exports = nextConfig;
