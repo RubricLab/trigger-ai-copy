cbc4fa6e2d7abac6934f57b5eb22980d54036026
tedspare <ted.spare@gmail.com>
2023-09-13 12:21:08 -0400

    Upgrade to GPT-4

--- jobs/generate.ts
+++ jobs/generate.ts
@@ -32,7 +32,7 @@ client.defineJob({
     const response = await io.openai.backgroundCreateChatCompletion(
       "openai-completions-api",
       {
-        model: "gpt-3.5-turbo",
+        model: "gpt-4",
         messages: [
           {
             role: "user",
