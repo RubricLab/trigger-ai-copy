0bc3e948636a4de61492e13bad88c0e197d5a442
tedspare <ted.spare@gmail.com>
2023-10-12 18:05:15 -0400

    Add background gradient

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Dashboard from "./components/Dashboard";
 import { Footer } from "./components/Footer";
+import { Gradients } from "./components/Gradients";
 import Header from "./components/Header";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="flex min-h-screen flex-col items-center justify-between relative">
       <Header />
       <Dashboard />
+      <Gradients />
       <Footer />
     </main>
   );
