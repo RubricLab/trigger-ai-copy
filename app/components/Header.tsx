c0d33aae5df32a1468b859c86f5e93c46efe44ce
tedspare <ted.spare@gmail.com>
2023-10-12 18:00:04 -0400

    Tweak navbar style

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,15 +1,15 @@
 import { GitHubLogoIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
 import React from "react";
-import TriggerLogo from "./TriggerLogo";
+import { TriggerLogo } from "./TriggerLogo";
 
 function Header() {
   return (
     <header className="w-screen h-20 border-b border-slate-800 fixed top-0 z-10 bg-midnight-900">
       <div className="flex items-center justify-between h-full px-12">
-        <div className="flex items-baseline gap-2">
-          <h1 className="text-2xl font-bold tracking-tighter">
-            CopyPrettifier
+        <div className="flex items-baseline gap-2 text-midnight-400">
+          <h1 className="text-2xl font-bold tracking-tighter bg-clip-text text-transparent bg-gradient-to-r from-indigo-600 to-purple-600">
+            LandingPageCopyGenerator
           </h1>
           <div className="text-xs flex items-center gap-1">
             by
