5d9a4a0496d52e81528be641b9a9df9ddab65ed1
tedspare <ted.spare@gmail.com>
2023-10-12 15:45:58 -0400

    Update atom styles

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,42 +1,54 @@
-import Link, { LinkProps } from "next/link";
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
+import { cn } from "../../utils";
 
-export type ButtonProps = {
-  children: React.ReactNode;
-  onClick?: (event: React.MouseEvent) => void;
-  disabled?: boolean;
-  className?: string;
-  type?: "button" | "submit" | "reset";
-} & React.HTMLAttributes<HTMLButtonElement>;
+const buttonVariants = cva(
+  "inline-flex items-center justify-center max-w-full rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  {
+    variants: {
+      variant: {
+        default: "bg-indigo-500 text-midnight-200 hover:bg-indigo-500/90",
+        destructive: "bg-red-600 text-red-200 hover:bg-red-600/90",
+        outline:
+          "border border-input bg-midnight-900 hover:bg-accent hover:text-midnight-200",
+        secondary:
+          "bg-midnight-750 bg-opacity-80 transition-opacity text-dimmed hover:bg-opacity-100",
+        ghost: "hover:bg-accent hover:text-midnight-200",
+        link: "text-primary underline-offset-4 hover:underline",
+      },
+      size: {
+        default: "h-10 px-4 py-2",
+        sm: "h-8 rounded-md px-3",
+        lg: "h-11 rounded-md px-8 !text-base",
+        icon: "h-10 w-10",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+);
 
-const classes =
-  "flex items-center justify-items-center justify-center w-full max-w-xs font-sans rounded-md transition-all disabled:opacity-50 disabled:pointer-events-none bg-indigo-600 hover:bg-indigo-500 h-10 px-4 font-semibold text-center";
-
-export function Button({
-  children,
-  onClick,
-  disabled,
-  className,
-  type = "button",
-}: ButtonProps) {
-  return (
-    <button
-      type={type}
-      onClick={onClick}
-      disabled={disabled}
-      className={`${classes} ${className}`}
-    >
-      {children}
-    </button>
-  );
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
 }
 
-export function ButtonLink({
-  children,
-  ...props
-}: LinkProps & { children: React.ReactNode }) {
-  return (
-    <Link {...props} className={classes}>
-      {children}
-    </Link>
-  );
-}
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : "button";
+    return (
+      <Comp
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+Button.displayName = "Button";
+
+export { Button, buttonVariants };
