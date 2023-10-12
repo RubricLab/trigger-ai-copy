41c71fea16de8b53a726aad91320871f5798173b
tedspare <ted.spare@gmail.com>
2023-10-12 18:01:36 -0400

    Add GitHub name to link

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,8 +6,8 @@ import { TriggerLogo } from "./TriggerLogo";
 function Header() {
   return (
     <header className="w-screen h-20 border-b border-slate-800 fixed top-0 z-10 bg-midnight-900">
-      <div className="flex items-center justify-between h-full px-12">
-        <div className="flex items-baseline gap-2 text-midnight-400">
+      <div className="flex items-center justify-between h-full px-12 text-midnight-400">
+        <div className="flex items-baseline gap-2">
           <h1 className="text-2xl font-bold tracking-tighter bg-clip-text text-transparent bg-gradient-to-r from-indigo-600 to-purple-600">
             LandingPageCopyGenerator
           </h1>
@@ -21,8 +21,10 @@ function Header() {
         <Link
           href="https://github.com/rubriclab/trigger-ai-copy"
           target="_blank"
+          className="flex space-x-2 items-center text-sm"
         >
           <GitHubLogoIcon className="w-6 h-6" />
+          <span>GitHub</span>
         </Link>
       </div>
     </header>
