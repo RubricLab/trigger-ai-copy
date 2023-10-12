497bbb6f086da1ecccac389f6afcd6bf6490f910
tedspare <ted.spare@gmail.com>
2023-10-12 14:45:04 -0400

    Persist browser instance with Durable Object

--- puppeteer/src/index.ts
+++ puppeteer/src/index.ts
@@ -1,7 +1,8 @@
 import puppeteer from "@cloudflare/puppeteer";
 
 export interface Env {
-	BROWSER: Fetcher;
+	FETCHER: Fetcher;
+	BROWSER: DurableObjectNamespace;
 	BUCKET: R2Bucket;
 	BUCKET_URL: string;
 }
@@ -11,6 +12,69 @@ export interface Env {
  */
 const worker = {
 	async fetch(request: Request, env: Env): Promise<Response> {
+		try {
+			// Forward request to the durable browser
+			let browserId = env.BROWSER.idFromName("DurableBrowser");
+			let durableBrowser = env.BROWSER.get(browserId);
+			const res = await durableBrowser.fetch(request);
+			const {
+				filename,
+				newHeadings,
+			}: {
+				filename: string;
+				newHeadings?: string[];
+			} = await res.json();
+
+			const fileUrl = `${env.BUCKET_URL}/${filename}${
+				newHeadings ? "-remixed" : ""
+			}.jpeg`;
+
+			return new Response(fileUrl);
+		} catch (error) {
+			console.error("Failed to reach browser", error);
+			return new Response("Failed to reach browser", { status: 500 });
+		}
+	},
+};
+
+export default worker;
+
+/**
+ * Durable Object to keep a browser session alive.
+ * Defined in same file because Cloudflare seems to throw an error otherwise:
+ * github.com/cloudflare/workers-sdk/issues/3738
+ */
+const secondsToLive = 60;
+const tenSeconds = 10 * 1000;
+export class DurableBrowser {
+	state: any;
+	env: Env;
+	secondsAlive: number;
+	storage: any;
+	browser: any;
+
+	constructor(state: any, env: Env) {
+		this.state = state;
+		this.env = env;
+		this.secondsAlive = 0;
+		this.storage = this.state.storage;
+	}
+
+	async fetch(request: Request) {
+		// Reuse open browser session if available
+		if (!this.browser) {
+			console.log("Browser: not found - starting new instance");
+			try {
+				this.browser = await puppeteer.launch(this.env.FETCHER);
+			} catch (e) {
+				console.log(`Browser: could not start instance. Error: ${e}`);
+				return new Response("Failed to start browser", { status: 500 });
+			}
+		}
+
+		// Reset keptAlive after each call to the browser
+		this.secondsAlive = 0;
+
 		const body: any = await request.json();
 
 		const { url: pageUrl, newHeadings, fullPage = false } = body;
@@ -20,24 +84,23 @@ const worker = {
 		}
 
 		const url = new URL(pageUrl).toString();
-		const filename = url.replace(/https:\/\//g, "").replace(/\.|\//g, "-");
+		const filename = url.replace(/https:\/\//g, "").replace(/\.|\//g, "");
 
 		try {
-			const browser = await puppeteer.launch(env.BROWSER);
-
-			const page = await browser.newPage();
+			const page = await this.browser.newPage();
 			await page.goto(url, { waitUntil: "networkidle0" });
 			await page.setViewport({
 				width: 1280,
 				height: 800,
 			});
 
+			// Loop over and replace headings if applicable
 			if (newHeadings) {
 				const headings = await page.$$("h1, h2, h3");
 				for (const element of newHeadings) {
 					if (element.id < headings.length) {
 						await page.evaluate(
-							(el, value) => (el.textContent = value),
+							(el: any, value: string) => (el.textContent = value),
 							headings[element.id],
 							element.text
 						);
@@ -51,26 +114,48 @@ const worker = {
 				type: "jpeg",
 			});
 
-			await env.BUCKET.put(
+			await this.env.BUCKET.put(
 				`${filename}${newHeadings ? "-remixed" : ""}.jpeg`,
 				screenshotBuffer
 			);
 
-			await browser.close();
+			// Reset keptAlive after performing tasks to the DO.
+			this.secondsAlive = 0;
+			let currentAlarm = await this.storage.getAlarm();
+			if (currentAlarm == null) {
+				console.log(`Browser: setting alarm`);
+				this.storage.setAlarm(Date.now() + tenSeconds);
+			}
 
 			return new Response(
-				`${env.BUCKET_URL}/${filename}${newHeadings ? "-remixed" : ""}.jpeg`,
+				JSON.stringify({
+					filename,
+					newHeadings,
+				}),
 				{
 					headers: {
-						"content-type": "text/plain",
+						"content-type": "application/json",
 					},
 				}
 			);
 		} catch (error) {
 			console.error("Failed to read page", error);
-			return new Response("Failed to read page", { status: 500 });
+			return new Response(JSON.stringify({ error: "Failed to read page" }), {
+				status: 500,
+			});
 		}
-	},
-};
+	}
 
-export default worker;
+	// Callback to extend browser life
+	async alarm() {
+		this.secondsAlive += 10;
+
+		if (this.secondsAlive < secondsToLive) {
+			console.log(`Browser: up for ${this.secondsAlive}s. Extending lifespan.`);
+			this.storage.setAlarm(Date.now() + tenSeconds);
+		} else {
+			console.log(`Browser: past ${secondsToLive}s life. Shutting down.`);
+			await this.browser.close();
+		}
+	}
+}
