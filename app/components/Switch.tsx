650fbf39d6395d996c34846bb3c64af841f2136c
tedspare <ted.spare@gmail.com>
2023-10-31 13:44:54 -0400

    Adjust switch colors

--- app/components/Switch.tsx
+++ app/components/Switch.tsx
@@ -16,7 +16,7 @@ const Switch = React.forwardRef<
       <SwitchPrimitives.Root
         id={id}
         className={cn(
-          "peer inline-flex bg-midnight-750 h-[24px] w-[44px] shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input",
+          "peer inline-flex group bg-midnight-600 hover:bg-midnight-700 transition-color h-[24px] w-[44px] shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input",
           className
         )}
         {...props}
@@ -25,7 +25,9 @@ const Switch = React.forwardRef<
         <SwitchPrimitives.Thumb
           className={cn(
             "pointer-events-none block h-5 w-5 rounded-full shadow-lg ring-0 transition-all data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0",
-            props.checked ? "bg-green-600" : "bg-midnight-500"
+            props.checked
+              ? "bg-green-600"
+              : "bg-midnight-400 group-hover:bg-midnight-300"
           )}
         />
       </SwitchPrimitives.Root>
