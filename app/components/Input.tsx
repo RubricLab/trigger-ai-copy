c2870542b1e83f61a2a33d59ab8cb8d40e3541c4
tedspare <ted.spare@gmail.com>
2023-10-31 14:13:41 -0400

    Adjust input width

--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -36,7 +36,7 @@ function Input({
   }, [value]);
 
   return (
-    <div className="space-y-0.5 w-full max-w-sm group">
+    <div className="space-y-0.5 w-full max-w-xs group">
       {label && (
         <label className="font-medium text-sm text-dimmed" htmlFor={inputId}>
           {label}
