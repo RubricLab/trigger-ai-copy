43c13dcbcae2038cb0a6c2f7c7217a8b9de284a0
tedspare <ted.spare@gmail.com>
2023-11-08 17:27:30 -0500

    Replace CF browser with Browserless

--- puppeteer/src/index.ts
+++ puppeteer/src/index.ts
@@ -13,85 +13,26 @@ export interface Env {
 const worker = {
 	async fetch(request: Request, env: Env): Promise<Response> {
 		try {
-			// Forward request to the durable browser
-			let browserId = await env.BROWSER.idFromName("browser");
-			let durableBrowser = await env.BROWSER.get(browserId);
+			const body: any = await request.json();
 
-			const res = await durableBrowser.fetch(request);
+			const { url: pageUrl, newHeadings, voice, fullPage = false } = body;
 
-			if (res.status === 500) {
-				const message = await res.text();
-				console.error("Screenshot failed: ", message);
-				return new Response(message, { status: 502 });
-			}
+			if (!pageUrl) return new Response("Please include a URL to visit");
 
-			const fileName: string = await res.text();
+			const url = new URL(pageUrl).toString();
+			const siteName = url.replace(/https:\/\//g, "").replace(/\.|\//g, "");
+			const fileName = `${siteName}${voice ? "-" + voice : ""}.jpeg`;
 			const fileUrl = `${env.BUCKET_URL}/${fileName}`;
 
-			return new Response(JSON.stringify({ fileUrl }));
-		} catch (error) {
-			console.error("Failed to reach browser", error);
-			return new Response("Failed to reach browser", { status: 500 });
-		}
-	},
-};
+			const cachedFile = await env.BUCKET.get(fileName);
+			// if (cachedFile && !newHeadings) return new Response(fileName);
 
-export default worker;
+			const browser = await puppeteer.connect({
+				browserWSEndpoint:
+					"wss://chrome.browserless.io?token=b334a293-a856-4079-b372-fc688fbb5959",
+			});
 
-/**
- * Durable Object to keep a browser session alive.
- * Defined in same file because Cloudflare seems to throw an error otherwise:
- * github.com/cloudflare/workers-sdk/issues/3738
- */
-const secondsToLive = 60;
-const tenSeconds = 10 * 1000;
-export class DurableBrowser {
-	state: any;
-	env: Env;
-	secondsAlive: number;
-	storage: any;
-	browser: any;
-
-	constructor(state: any, env: Env) {
-		this.state = state;
-		this.env = env;
-		this.secondsAlive = 0;
-		this.storage = this.state.storage;
-	}
-
-	async fetch(request: Request) {
-		// Reuse open browser session if available
-		if (!this.browser) {
-			console.log("Browser: not found - starting new instance");
-			try {
-				this.browser = await puppeteer.launch(this.env.FETCHER);
-			} catch (e) {
-				console.log(`Browser: could not start instance. Error: ${e}`);
-				return new Response("Failed to start browser", { status: 500 });
-			}
-		}
-
-		// Reset keptAlive after each call to the browser
-		this.secondsAlive = 0;
-
-		const body: any = await request.json();
-		const { url: pageUrl, newHeadings, voice, fullPage = false } = body;
-
-		if (!pageUrl) {
-			return new Response("Please include a URL to visit");
-		}
-
-		const url = new URL(pageUrl).toString();
-		const siteName = url.replace(/https:\/\//g, "").replace(/\.|\//g, "");
-		const fileName = `${siteName}${voice ? "-" + voice : ""}.jpeg`;
-
-		const cachedFile = await this.env.BUCKET.get(fileName);
-		if (cachedFile && !newHeadings) {
-			return new Response(fileName);
-		}
-
-		try {
-			const page = await this.browser.newPage();
+			const page = await browser.newPage();
 
 			await page.setViewport({ width: 1280, height: 960 });
 			await page.goto(url, {
@@ -121,33 +62,16 @@ export class DurableBrowser {
 				type: "jpeg",
 			});
 
-			await this.env.BUCKET.put(fileName, screenshotBuffer);
+			await env.BUCKET.put(fileName, screenshotBuffer);
 
-			// Reset keptAlive after performing tasks to the DO.
-			this.secondsAlive = 0;
-			let currentAlarm = await this.storage.getAlarm();
-			if (currentAlarm == null) {
-				console.log("Browser: setting alarm");
-				await this.storage.setAlarm(Date.now() + tenSeconds);
-			}
+			await browser.close();
 
-			return new Response(fileName);
+			return new Response(JSON.stringify({ fileUrl }));
 		} catch (error) {
-			console.error(`Failed to read page: ${error}`);
-			return new Response(`Failed to read page: ${error}`, { status: 500 });
+			console.error("Failed to reach browser", error);
+			return new Response("Failed to reach browser", { status: 500 });
 		}
-	}
+	},
+};
 
-	// Callback to extend browser life
-	async alarm() {
-		this.secondsAlive += 10;
-
-		if (this.secondsAlive < secondsToLive) {
-			console.log(`Browser: up for ${this.secondsAlive}s. Extending lifespan.`);
-			await this.storage.setAlarm(Date.now() + tenSeconds);
-		} else {
-			console.log(`Browser: past ${secondsToLive}s life. Shutting down.`);
-			await this.browser?.close?.();
-		}
-	}
-}
+export default worker;
