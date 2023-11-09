67bb012f6fc06183436c4ee17625295e4885fa01
tedspare <ted.spare@gmail.com>
2023-11-09 14:41:18 -0500

    Move voices to new file

---
+++ app/constants.ts
@@ -0,0 +1,11 @@
+export const voices = {
+  pirate: { label: "🏴‍☠️ Pirate", value: "a pirate" },
+  shakespeare: { label: "🎭 Shakespeare", value: "William Shakespeare" },
+  rhyming: { label: "🎤 Rhyming", value: "rhyming" },
+  yoda: { label: "🧙 Yoda", value: "Master Yoda" },
+  cuss: {
+    label: "🤬 F*ck it!",
+    value: "cuss words (use a*terisks to make it safe)",
+  },
+  useful: { label: "✨ Useful", value: "useful" },
+};
