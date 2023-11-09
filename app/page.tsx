2a6890f2def1de695a2d65d0f2a1a5282859e883
tedspare <ted.spare@gmail.com>
2023-11-09 13:34:28 -0500

    Pass URL params to dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,18 @@ import { Footer } from "./components/Footer";
 import { Gradients } from "./components/Gradients";
 import Header from "./components/Header";
 
-export default function Home() {
+type Props = {
+  searchParams: {
+    url?: string;
+    voice?: string;
+  };
+};
+
+export default function Home({ searchParams }: Props) {
   return (
     <main className="flex min-h-screen pb-16 flex-col items-center justify-between relative">
       <Header />
-      <Dashboard />
+      <Dashboard url={searchParams?.url} voice={searchParams?.voice} />
       <Gradients />
       <Footer />
     </main>
